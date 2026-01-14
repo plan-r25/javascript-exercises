92c419664f8dedfe1905aa9a28d144d7b8ffd5c4
planR <rplann24@gmail.com>
2026-01-14 09:56:37 +0000

    Add an if statement, and a for loop

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(message, n) {
+  if ( n < 0 ) return 'ERROR';
+  let repeat = '';
+  for (let i = 0; i < n; i++ ) {
+     repeat += message;
+  }
+  return repeat;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
