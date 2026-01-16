6016524efa804d5e7e00e171f19e92c85795808a
planR <rplann24@gmail.com>
2026-01-16 10:10:11 +0000

    We use a rest operator to be able to remove multiple elements with filter

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...valueToRemove) {
+  return arr = array.filter(e => !valueToRemove.includes(e));
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;
