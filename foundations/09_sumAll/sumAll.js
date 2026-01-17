67580508336beb4d7d08d561146318b8b42d0b81
planR <rplann24@gmail.com>
2026-01-17 10:51:37 +0000

    we first for non-integers and then implement a for loop

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
-};
+const sumAll = function(first, last) {
+ if (!Number.isInteger(first) || !Number.isInteger(last)) return 'ERROR';
+ if(first < 0 || last < 0) return 'ERROR';
+ if (first > last) {
+    const temp = first;
+    first = last;
+    last = temp;
+  }
+  let sum =0;
+  for (let i = first; i <= last; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
