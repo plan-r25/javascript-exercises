57a242b1dddfe821b01fa8dcc971475795e08651
planR <rplann24@gmail.com>
2026-01-20 10:01:08 +0000

    in 11, we add their formula and then round them to get a decimal

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+ const tempt = (f - 32) * 5/9;
+ return celsius = Math.round(tempt * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const tempt  = (c * 9/5) + 32;
+  return fahrenheit = Math.round(tempt * 10) / 10;
 };
+
 
 // Do not edit below this line
 module.exports = {
