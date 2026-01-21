476b4cb84d0af95c6959d1232124f59c4dbf9551
planR <rplann24@gmail.com>
2026-01-21 09:30:36 +0000

    12, working with simple logic

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(number) {
+  let result = 0;
+  for (let i = 0; i < number.length; i++) {
+    result += number[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = 1;
+  for (let i = 0; i < numbers.length; i++) {
+    result *= numbers[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+   return a ** b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;	
 };
 
 // Do not edit below this line
