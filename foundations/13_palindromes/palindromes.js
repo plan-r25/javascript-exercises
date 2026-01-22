66d6a645c61f446df9f9180f787e244516a2b973
planR <rplann24@gmail.com>
2026-01-22 03:39:12 +0000

    we first convert it to a cleanStr and then compare the cleanStr with the reverseStr

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (string) {
+const cleanStr = string.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+return cleanStr === cleanStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
