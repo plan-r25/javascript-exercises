d181d8738f0c40a327a901ac5528ab6e0888bed9
planR <rplann24@gmail.com>
2026-01-22 08:54:42 +0000

    14, first we check for negative n, and implement a state update inside the for loop

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) return 'OOPS';
+  if (n < 1) return 0;
 
-};
+  let sum;
+  let prev = 0;
+  let curr = 1;
+
+  for (let i = 2; i <= n; i++) {
+    sum = prev + curr;
+    prev = curr;
+    curr = sum;
+  }
+  return curr;
+}
+  
 
 // Do not edit below this line
 module.exports = fibonacci;
