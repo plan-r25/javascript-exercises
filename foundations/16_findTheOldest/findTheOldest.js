1e54566b3832a13c7b72a3011b7dd4516535c4ca
planR <rplann24@gmail.com>
2026-01-24 05:59:51 +0000

    16, we use the reduce to get one final answer

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  return array.reduce((oldest, curr) => {
+    const currentYear = new Date().getFullYear();
 
+    const getAge = (p) => (p.yearOfDeath ?? currentYear) - p.yearOfBirth;
+
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(curr);
+
+    return currentAge > oldestAge ? curr : oldest;
+  });
 };
+
+/*  const currentYear = new Date().getFullYear();
+  const peopleAge = array.map((person) => {
+    const death = person.yearOfDeath || currentYear;
+    return {
+      ...person,
+      age: death - person.yearOfBirth
+    }
+  })
+  return peopleAge.reduce((oldest, curr) => {
+    return curr.age > oldest.age ? curr : oldest;
+  })
+}; */
 
 // Do not edit below this line
 module.exports = findTheOldest;
